755773492025c0239b7e19bcee64f078ddfcec51
paarthd00 <paarthcode@gmail.com>
2024-03-15 15:24:09 -0700

    add backend routes for ai chat completion

--- frontend/src/components/custom-editor.tsx
+++ frontend/src/components/custom-editor.tsx
@@ -1,10 +1,15 @@
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
-
+import { useTheme } from "next-themes";
 export default function CustomEditor() {
+  const { theme } = useTheme();
   return (
     <div>
       <Editor
-        theme="vs-dark"
+        theme={
+          theme === "dark"
+            ? "vs-dark"
+            : "vs-light"
+        }
         height="90vh"
         defaultLanguage="javascript"
         defaultValue="// some comment"
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,48 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import OpenAI from "openai";
 
-const app = new Hono()
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-Bun.serve(app)
+app.get("/", (c) => {
+  return c.text("Hello This is the tmp code backend server!");
+});
 
-// export default app
+app.post("/openai/code", async (c) => {
+  const { prompt } = await c.req.json();
+  const { language } = await c.req.json();
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content:
+          prompt +
+          " please provide code nothing else, just code in" +
+          language +
+          " language",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  return c.json({ message: chatCompletion.choices[0].message.content });
+});
+
+app.post("/openai/explain", async (c) => {
+  const { prompt } = await c.req.json();
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt + " Please explain this to me",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  return c.json({ message: chatCompletion.choices[0].message.content });
+});
+
+Bun.serve(app);
